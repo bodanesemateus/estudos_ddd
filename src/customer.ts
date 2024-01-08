56122463230ba4ac8edec408a1ce819077c388f9
bodanesemateus <bodanesemateus@gmail.com>
2024-01-08 08:54:23 -0300

    criacao de metodos para alteracoes do Customer

--- src/customer.ts
+++ src/customer.ts
@@ -2,22 +2,29 @@ class Customer {
     _id: string;
     _name: string;
     _address: string;
+    _active: boolean;
 
     constructor(id: string, name: string, address: string) {
         this._id = id;
         this._name = name;
         this._address = address;
+        this._active = true;
     }
 
-    get id(): string {
-        return this._id;
+    changeName(newName: string) {
+        this._name = newName;
     }
 
-    get name(): string {
-        return this._name;
+    changeAddress(newAddress: string) {
+        this._address = newAddress;
     }
 
-    get address(): string {
-        return this._address;
+    deactivate() {
+        this._active = false;
     }
+
+    activate() {
+        this._active = true;
+    }
+
 }
