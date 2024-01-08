241e480a341e559f1abcb904f9aa1748f35b59f8
bodanesemateus <bodanesemateus@gmail.com>
2024-01-08 16:56:11 -0300

    ajuste na chamada do OrdemItem

--- src/main.ts
+++ src/main.ts
@@ -5,11 +5,11 @@ import OrderItem from "./entity/order_item";
 
 let customer = new Customer('1', 'Mateus Bodanese');
 let address = new Address('Rua 1', 123, '12345-123', 'Cidade 1');
-customer.address = address;
+customer.Address = address;
 customer.activate();
 
-const item1 = new OrderItem('1', 'Item 1', 10);
-const item2 = new OrderItem('2', 'Item 2', 15);
-const item3 = new OrderItem('3', 'Item 3', 20);
+const item1 = new OrderItem('1', 'Item 1', 10, 1, '1');
+const item2 = new OrderItem('2', 'Item 2', 15, 2, '2');
+const item3 = new OrderItem('3', 'Item 3', 20, 3, '3');
 
 const order = new Order('1', '123', [item1, item2, item3]);
