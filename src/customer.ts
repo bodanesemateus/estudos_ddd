3dff7f700cc7a04b8ce647b2520ea9ebc88c9e43
bodanesemateus <bodanesemateus@gmail.com>
2024-01-08 10:33:35 -0300

    criado funcao para validar os dados e adicionado comentarios importantes

--- src/customer.ts
+++ src/customer.ts
@@ -1,3 +1,27 @@
+// uma entidade SEMPRE deve ser autovalidável, garantindo que ela sempre estará em um estado válido
+
+// ORM foca em persistência de dados, não em regras de negócio
+
+// regra de negócio fica na entidade
+
+// de resumo, voce tem uma entidade que e para regra de negocio 
+// e uma entidade que e para persistencia de dados
+
+// nunca, jamais, trate regra de negócio na entidade de persistência de dados
+
+// por mais que todos os campos sejam alteraveis, sempre trate em dois arquivos diferentes
+
+// domain
+   // entities
+      // customer.ts
+         // aqui se resolve regra de negócio
+
+// infra 
+   // db
+      // model
+         // customer.ts
+            // aqui tem get e set e armazena no banco de dados
+
 class Customer {
     _id: string;
     _name: string;
@@ -11,20 +35,42 @@ class Customer {
         this._active = true;
     }
 
+    validate() {
+        if (!this._id || this._id.length === 0) {
+            throw new Error('Invalid id');
+        }
+     
+        if (!this._name || this._name.length === 0 || this._name.length < 3) {
+            throw new Error('Invalid name');
+        }
+
+        if (!this._address || this._address.length === 0) {
+            throw new Error('Invalid address');
+        }
+
+    }
+
     changeName(newName: string) {
         this._name = newName;
+        this.validate();
     }
 
     changeAddress(newAddress: string) {
         this._address = newAddress;
+        this.validate();
     }
 
     deactivate() {
         this._active = false;
+        this.validate();
     }
 
     activate() {
+        if (this._address.length === 0) {
+            throw new Error('Address is required to activate customer');
+        } 
         this._active = true;
+        this.validate();
     }
 
 }
