d3420449bd63abb422788958865239fb0fc6ce3b
bodanesemateus <bodanesemateus@gmail.com>
2024-01-10 10:52:51 -0300

    Inclusao de testes do ordem spec

--- src/infra/repository/order.repository.ts
+++ src/infra/repository/order.repository.ts
@@ -6,14 +6,14 @@ export default class OrderRepository {
     async create(entity: Order): Promise<void>{
         await OrderModel.create({
             id: entity.id,
-            customerId: entity.customerId,
+            customer_id: entity.customerId,
             total: entity.total(),
             items: entity.items.map(item => ({
                 id: item.id,
                 name: item.name,
                 price: item.price,
                 quantity: item.quantity,
-                productId: item.productId
+                product_id: item.productId
             }))
         },
         {
